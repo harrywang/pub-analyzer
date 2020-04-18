963f23e9e5f89a0e2c1893d2dbfa8b3bb20f80a1
Eric Zhang <57052113+mianhu888@users.noreply.github.com>
2020-04-18 09:50:28 -0400

    Create keywords.py

---
+++ keywords.py
@@ -0,0 +1,3 @@
+INSTITUTES = ['school', 'university', 'department','college','management','systems','center','institute']
+
+COUNTRIES = ['u.s.a.', 'united kingdom','canada', 'france','netherlands','norway','switzerland','sweden','china','india','singapore','south korea','australia','spain','hong kong','taiwan']
