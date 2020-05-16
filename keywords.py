66134c6c6d1e9626e11d77ed5f4393d4806680f0
Harry Wang <harryjwang@gmail.com>
2020-05-16 13:37:37 -0400

    some refactoring

--- keywords.py
+++
@@ -1,3 +0,0 @@
-INSTITUTES = ['school', 'university', 'department','college','management','systems','center','institute']
-
-COUNTRIES = ['u.s.a.', 'united kingdom','canada', 'france','netherlands','norway','switzerland','sweden','china','india','singapore','south korea','australia','spain','hong kong','taiwan']
